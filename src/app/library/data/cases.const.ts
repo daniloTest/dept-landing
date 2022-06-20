8c3446ed179f03bd5f287fcaba0ee2866629d4ed
Danilo Colella <danilocolella17@gmail.com>
2022-06-20 21:23:23 +0200

    - [FE] add case images and brand logos

--- src/app/library/data/cases.const.ts
+++ src/app/library/data/cases.const.ts
@@ -3,107 +3,107 @@ import { CaseReadDto } from "../interfaces/case.dto";
 export const CASES_DATA: CaseReadDto[] = [
   {
     routerLink: 'XX_1',
-    imageUrl: 'assets/images/cases/image__case-1.jpg',
     title: 'A Summer island in the Netherlands',
     category: 'WORK',
-    industry: 'BOL.COM'
+    industry: 'BOL.COM',
+    imageUrl: 'assets/images/cases/image__case-1.jpg',
   },
   {
     routerLink: 'XX_1',
     title: 'Not some average banking website',
     category: 'LEISURE',
     industry: 'KEMPEN',
-    imageUrl: 'assets/images/cases/image__case-1.jpg'
+    imageUrl: 'assets/images/cases/image__case-2.jpg'
   },
   {
     routerLink: 'XX_1',
-    imageUrl: 'assets/images/cases/image__case-1.jpg',
     title: 'A Summer island in the Netherlands',
     category: 'WORK',
-    industry: 'BOL.COM'
+    industry: 'BOL.COM',
+    imageUrl: 'assets/images/cases/image__case-3.jpg',
   },
   {
     routerLink: 'XX_1',
     title: 'Not some average banking website',
     category: 'LEISURE',
     industry: 'KEMPEN',
-    imageUrl: 'assets/images/cases/image__case-1.jpg'
+    imageUrl: 'assets/images/cases/image__case-4.jpg'
   },
   {
     routerLink: 'XX_1',
-    imageUrl: 'assets/images/cases/image__case-1.jpg',
     title: 'A Summer island in the Netherlands',
     category: 'WORK',
-    industry: 'BOL.COM'
+    industry: 'BOL.COM',
+    imageUrl: 'assets/images/cases/image__case-5.jpg',
   },
   {
     routerLink: 'XX_1',
     title: 'Not some average banking website',
     category: 'WORK',
     industry: 'KEMPEN',
-    imageUrl: 'assets/images/cases/image__case-1.jpg'
+    imageUrl: 'assets/images/cases/image__case-6.jpg'
   },
   {
     routerLink: 'XX_1',
-    imageUrl: 'assets/images/cases/image__case-1.jpg',
     title: 'A Summer island in the Netherlands',
     category: 'WORK',
-    industry: 'BOL.COM'
+    industry: 'BOL.COM',
+    imageUrl: 'assets/images/cases/image__case-7.jpg',
   },
   {
     routerLink: 'XX_1',
     title: 'Not some average banking website',
     category: 'WORK',
     industry: 'KEMPEN',
-    imageUrl: 'assets/images/cases/image__case-1.jpg'
+    imageUrl: 'assets/images/cases/image__case-8.jpg'
   },
   {
     routerLink: 'XX_1',
-    imageUrl: 'assets/images/cases/image__case-1.jpg',
     title: 'A Summer island in the Netherlands',
     category: 'WORK',
-    industry: 'BOL.COM'
+    industry: 'BOL.COM',
+    imageUrl: 'assets/images/cases/image__case-9.jpg',
   },
   {
     routerLink: 'XX_1',
     title: 'Not some average banking website',
     category: 'WORK',
     industry: 'KEMPEN',
-    imageUrl: 'assets/images/cases/image__case-1.jpg'
+    imageUrl: 'assets/images/cases/image__case-10.jpg'
   },
   {
     routerLink: 'XX_1',
     title: 'Not some average banking website',
     category: 'WORK',
     industry: 'KEMPEN',
-    imageUrl: 'assets/images/cases/image__case-1.jpg'
+    imageUrl: 'assets/images/cases/image__case-11.jpg'
   },
   {
     routerLink: 'XX_1',
-    imageUrl: 'assets/images/cases/image__case-1.jpg',
     title: 'A Summer island in the Netherlands',
     category: 'WORK',
-    industry: 'BOL.COM'
+    industry: 'BOL.COM',
+    imageUrl: 'assets/images/cases/image__case-12.jpg',
   },
   {
     routerLink: 'XX_1',
     title: 'Not some average banking website',
     category: 'WORK',
     industry: 'KEMPEN',
-    imageUrl: 'assets/images/cases/image__case-1.jpg'
+    imageUrl: 'assets/images/cases/image__case-13.jpg'
   },
   {
     routerLink: 'XX_1',
-    imageUrl: 'assets/images/cases/image__case-1.jpg',
     title: 'A Summer island in the Netherlands',
     category: 'WORK',
-    industry: 'BOL.COM'
+    industry: 'BOL.COM',
+    imageUrl: 'assets/images/cases/image__case-11.jpg',
   },
   {
     routerLink: 'XX_1',
     title: 'Not some average banking website',
     category: 'WORK',
     industry: 'KEMPEN',
-    imageUrl: 'assets/images/cases/image__case-1.jpg'
+    imageUrl: 'assets/images/cases/image__case-10.jpg'
   }
 ];
